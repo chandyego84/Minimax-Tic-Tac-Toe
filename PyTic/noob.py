b260780511fe65fa53fea95cce3d9b6683ef64aa
chandyego <chandlerjuego101@gmail.com>
2022-10-25 21:20:23 -0700

    starting noob python

---
+++ PyTic/noob.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def main():
+    # basic array stuff
+    #a = np.array([1, 2, 3, 4])
+    #b = np.array([[1, 2, 3], [1, 2, 3]])
+    #print(a)
+    #print(b)
+
+    # basic zeros array/ones array
+    #print(np.zeros(2))
+    #print(np.ones(2))
+
+    # range of elements array
+    #print(np.arange(4))
+    #print(np.arange(2, 9, 2))
+    #print(np.linspace(0, 10, num=5)) # to the fifth decimal
+
+    #x = np.ones(2, dtype=np.int64)
+    #print(x)
+
+    # adding, removing sorting elements
+    #arr = np.array([42, 3, 13, 69])
+    #arr = np.sort(arr)
+    #print(arr)
+
+    #a = np.arange(5)
+    #b = np.arange(5, 10)
+    #c = np.concatenate((a, b))
+    #print(c)
+
+    #x = np.array([[1, 2], [3, 4]])
+    #y = np.array([[5, 6]])
+    #z = np.concatenate((x, y), 0)
+    #print(z)
+
+    # shape and size of an array
+    
+
+
+    print("I am a numpy noob")
+    
+
+if __name__ == '__main__':
+    main()
